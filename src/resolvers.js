57f4e7e02876b68d8658e903fc9ed81c053cc40a
Ayush Jain <ayushjain0023>
2022-03-17 14:26:26 +0530

    Added new field duraion in seconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -44,6 +44,10 @@ const resolvers = {
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    durationInSecond: ({ length }) => length,
+  },
+  Module: {
+    durationInSecond: ({ length }) => length,
   },
 };
 
